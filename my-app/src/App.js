cdacb489491a7a51defb2e7d360e12d8bb45bb6d
tugbasirguvec <tugbasirguvec@gmail.com>
2024-07-19 20:21:37 +0300

    merge conflict ucuncu branch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Merge conflict ucuncu branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Merge conflict ucuncu branch</p>
       </header>
     </div>
   );
